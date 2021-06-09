26ff01f38ecf37eca09703962ee095537ca28436
jasonleyser <jason.leyser@protocol.ai>
2021-06-08 18:08:44 -0600

    initial commit
    Api endpoint for a twitter accounts most recent media image

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import useSwr from 'swr'
+import Link from 'next/link'
+
+export default function Index() {
+  return (
+    <div style={{ position: 'absolute', backgroundColor: "#fdfdfd", width: '100vw', height: '100vh', top: '0', left: '0' }}>
+      <div style={{ position: 'absolute', width: '40px', height: '40px', left: '48%', top: '48%' }}>
+        <svg
+          viewBox="0 0 447 516"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M446.353 386.57L362.353 435.038V391.124L408.353 364.528V269.318L222.853 162.225V118.312L446.353 247.363V386.57ZM408.353 151.624V204.624L446.353 226.624V129.624L222.853 0.624146L102.353 70.1241V210.463L306.353 328.124V423.455L222.853 471.624L38.3531 365.124V312.124L0.353149 290.124V387.124L222.853 515.624L344.353 445.43V306.277L140.353 188.456V92.0493L222.853 44.4236L408.353 151.624ZM0.353149 129.124L84.3531 80.4781V124.419L38.3531 151.124V247.369L222.853 353.863V397.789L0.353149 269.323V129.124Z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+/*
+const users = [
+  { account: 'archillect', freq: 'Hourly' }, 
+  { account: 'filecoin', freq: 'Daily' }, 
+]
+export default function Index() {
+    return (
+        <div style={{ position: 'absolute', backgroundColor: "#fdfdfd", width: '100vw', height: '100vh', top: '0', left: '0' }}>
+      <div style={{ position: 'absolute', width: '200px', height: 'auto', left: '48%', top: '48%' }}>
+        <table>
+          {users.map((user) => (
+            <td>@${user.account}</td>
+            <td>${user.freq}</td>
+          ))}
+        </table>
+      </div>
+    </div>
+    )
+}
+*/
