0e3a8ffeeac36a583c079979ff917b77b1f1cf63
jasonleyser <jason.leyser@protocol.ai>
2021-06-09 11:37:31 -0600

    Updated cron list

--- pages/list.js
+++ pages/list.js
@@ -1,23 +1,22 @@
 const users = [
-    { account: 'archillect', freq: 'hourly', run_time: ':25' },
-]
-
+    { account: 'archillect', collection: 'photos', freq: 'hourly', run_time: 'xx:25' },
+];
 
 export default function Index() {
     return (
-      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+      <div style={{ position: 'absolute', backgroundColor: "#fdfdfd", top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
         <div>
-          <table style={{ width: '800px', borderCollapse: 'collapse', overflow: 'hidden', boxShadow: '0 0 20px rgba(0,0,0,0.1)' }}>
+          <table style={{ width: '800px', border: '1px solid #fdfdfd', borderCollapse: 'collapse', overflow: 'hidden', boxShadow: '0 0 20px rgba(0,0,0,0.1)' }}>
             <thead>
-              <tr>
+              <tr style={{ borderBottom: '1px solid #cacaca' }}>
                 <th>Account</th>
                 <th>Frquency</th>
-                <th>Run time</th>
+                <th>Runs at</th>
               </tr>
             </thead>
             {users.map((user) => (
-              <tr>
-                <th><a target="_blank" href={`https://twitter.com/${user.account}`}>@{user.account}</a></th>
+              <tr style={{ padding: '8px' }}>
+                <th><a target="_blank" href={`https://slate.host/${user.account}/${user.collection}`}>@{user.account}</a></th>
                 <th>{user.freq}</th>
                 <th>{user.run_time}</th>
               </tr>
