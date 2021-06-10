a0cde36085bb17f722ee5d8ce547552b4d5b9c92
jasonleyser <jason.leyser@protocol.ai>
2021-06-10 10:23:28 -0600

    Style updates

--- pages/list.js
+++ pages/list.js
@@ -1,33 +1,41 @@
+import Head from 'next/head'
+
 const users = [
-    { account: 'archillect', collection: 'photos', freq: 'hourly', run_time: 'xx:25' },
-    { account: 'wayback', collection: 'exe', freq: '2/day', run_time: '12:30, 6:30' },
+    { account: 'archillect', collection: 'photos', freq: 'hourly', run_time: 'xx:26' },
+    { account: 'wayback', collection: 'exe', freq: '6 hours', run_time: '2:22, 8:22' },
+    { account: '90snba', collection: 'photos', freq: '6 hours', run_time: '10:00, 6:00' },
+    { account: 'moma', collection: 'objects', freq: '4 hours', run_time: '10:05, 2:05, 6:05' },
 ];
 
-export default function Index() {
+export default function List() {
     return (
-      <div style={{ position: 'absolute', backgroundColor: "#fdfdfd", top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-        <div>
-          <table style={{ width: '800px', border: '1px solid #fdfdfd', borderCollapse: 'collapse', overflow: 'hidden', boxShadow: '0 0 20px rgba(0,0,0,0.1)' }}>
-            <thead>
-              <tr style={{ borderBottom: '1px solid #cacaca' }}>
-                <th>Account</th>
-                <th>Frquency</th>
-                <th>Runs at (MT)</th>
-              </tr>
-            </thead>
-            {users.map((user) => (
-              <tr style={{ paddingBottom: '1em' }}>
-                <th>
-                  <a target="_blank" href={`https://slate.host/${user.account}/${user.collection}`}>
-                    @{user.account}/{user.collection}
-                  </a>
-                </th>
-                <th>{user.freq}</th>
-                <th>{user.run_time}</th>
-              </tr>
-            ))}
-          </table>
+      <>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Inter" rel="stylesheet" />
+        </Head>
+
+        <div style={{ position: 'absolute', backgroundColor: "#fdfdfd", top: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontFamily: 'Inter' }}>
+          <div>
+            <table style={{ width: '500px', border: '1px solid #fdfdfd', borderCollapse: 'collapse', overflow: 'hidden', boxShadow: '0 0 20px rgba(0,0,0,0.1)', borderRadius: '4px' }}>
+              <thead>
+                <tr style={{ borderBottom: '1px solid #cacaca' }}>
+                  <th>Account</th>
+                  <th>Frquency</th>
+                </tr>
+              </thead>
+              {users.map((user) => (
+                <tr style={{ borderSpacing: '20px' }}>
+                  <th>
+                    <a target="_blank" href={`https://slate.host/${user.account}/${user.collection}`}>
+                      @{user.account}/{user.collection}
+                    </a>
+                  </th>
+                  <th>{user.freq}</th>
+                </tr>
+              ))}
+            </table>
+          </div>
         </div>
-      </div>
+      </>
     )
 }
